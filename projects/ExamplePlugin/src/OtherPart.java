d948e29f881553cb150f50f20d3867e5b9ef82f7
A Binns <abinns@users.noreply.github.com>
2014-04-04 16:23:44 -0400

    Simple example plugin, does basically nothing, exsists to be exported to a jar file and used from there.

---
+++ projects/ExamplePlugin/src/OtherPart.java
@@ -0,0 +1,11 @@
+import abinns.appserv.appmanager.AppContext;
+import abinns.appserv.backend.U;
+
+
+public class OtherPart
+{
+	public OtherPart(AppContext bob)
+	{
+		U.p(bob + " From somewhere Else!!!");
+	}
+}
